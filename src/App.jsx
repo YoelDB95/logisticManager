9b01c85c6f93872e0f99c54c3ced838a41f74a8c
Yoel <yoeldelbuono@outlook.com>
2026-06-24 23:25:02 -0300

    Change name of dashboard component to Delivery

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Header } from './components/Header.jsx'
 import { FormLoadPackage } from './components/FormLoadPackage.jsx'
-import { Dashboard } from './components/Dashboard.jsx'
+import { Delivery } from './components/Delivery.jsx'
 import MLScanner from './components/MLScanner.jsx'
 import OCRCamera from './components/OCRCamera.jsx'
 import MLScannerCrop from './components/MLScannerCrop.jsx'
@@ -22,9 +22,9 @@ function App() {
   return (
     <>
       <Header />
-      {/*<FormLoadPackage packages={packages} setPackages={setPackages}/>*/}
-      {/*<Dashboard packages={packages}/>*/}
-      {<MLScannerCrop />}
+      {/*<FormLoadPackage packages={packages} setPackages={setPackages}/> /* Hacer link a Cargar Paquetes en el Header */} 
+      {<Delivery packages={packages}/> /* Hacer link a Envios en el Header */}
+      {/*<MLScannerCrop />*/}
     </>
   )
 }
